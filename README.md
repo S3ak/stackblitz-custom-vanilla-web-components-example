e9528502b62220fb7e86854af1bb9d2858c441ea
S3ak <nsineke@gmail.com>
2024-08-30 10:00:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-custom-vanilla-web-components-example
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/S3ak/stackblitz-custom-vanilla-web-components-example)
---
+++ my-component/my-component.js
@@ -0,0 +1,30 @@
+class MyComponent extends HTMLElement {
+  constructor() {
+    super(); // Always call super() first in the constructor.
+    // Element functionality written in here.
+    this.action = this.action.bind(this);
+  }
+
+  // NOTE: Runs at the start of the component
+  connectedCallback() {
+    this.innerHTML = `
+    <section>
+      <p>This is a web component!</p>
+      <button id="js-btn">Click Me</button>
+    </section>
+    `; // Setting the HTML template
+    this.querySelector('#js-btn').addEventListener('click', this.action);
+  }
+
+  // NOTE: Runs at the end of the component life cycle
+  disconnectedCallback() {
+    this.querySelector('#js-btn').removeEventListener('click', this.action);
+  }
+
+  action() {
+    console.warn('>>> CLICKED!!! <<<');
+  }
+}
+
+// Registers the custom element with the browser's Custom Elements registry
+customElements.define('my-component', MyComponent);
